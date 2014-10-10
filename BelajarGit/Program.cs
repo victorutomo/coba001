1e3fb3af7ae639d38566df636e30400024a3d3b5
samuelagus <samuel.agus@ngawur.com>
2014-10-10 08:57:13 +0700

    Edit Program.cs

--- BelajarGit/Program.cs
+++ BelajarGit/Program.cs
@@ -10,6 +10,7 @@ namespace BelajarGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            (new CGlobal()).Write();
             Console.ReadKey();
         }
     }
